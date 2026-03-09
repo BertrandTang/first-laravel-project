ae8e6f180f12ad0f66a5bf02c02e2d4af5a65cd7
Bertrand <bertrand.tang.dev@gmail.com>
2026-03-09 16:05:40 +0100

    Activité 6 : Créer un ArticleController qui renvoie un article suivi d'un ID à la route article/id

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/', [IndexController::class, 'index']);
+Route::get('/article/{id}', [ArticleController::class, 'show']);
