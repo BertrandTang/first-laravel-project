628c52187fd81ec01741374f32e2bdd65516efc5
Bertrand <bertrand.tang.dev@gmail.com>
2026-03-09 17:07:10 +0100

    structured not-found.blade

--- resources/views/errors/not-found.blade.php
+++ resources/views/errors/not-found.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <h1>Page non trouvée</h1>
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>404</title>
+    <body>
+        <h1>Page non trouvée</h1>
+    </body>
+</html>
