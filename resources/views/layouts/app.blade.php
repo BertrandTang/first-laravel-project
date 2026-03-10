d1ec8d39e558521134f7a6d47468adb3b3e1fcaf
Bertrand <bertrand.tang.dev@gmail.com>
2026-03-10 16:42:46 +0100

    Activité 15 et 16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,15 @@
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
         <head>
             <meta charset="UTF-8">
+            <link rel="icon" href="{{ asset('favicon.ico') }}">
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
             <title>@yield('title')</title>
-        </head>
-        <body>
-                @include('partials.header')
-            <main>
-                @yield('content')
-            </main>
-                @include('partials.footer')
-    </body>
+            </head>
+            <body>
+                    @include('partials.header')
+                <main>
+                    @yield('content')
+                </main>
+                    @include('partials.footer')
+        </body>
 </html>
