e574ae02dd6f1dd39f553a16967a37e76a5d3d8c
Bertrand <bertrand.tang.dev@gmail.com>
2026-03-10 16:44:12 +0100

    linting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-        <head>
-            <meta charset="UTF-8">
-            <link rel="icon" href="{{ asset('favicon.ico') }}">
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-            <title>@yield('title')</title>
+            <head>
+                <meta charset="UTF-8">
+                <link rel="icon" href="{{ asset('favicon.ico') }}">
+                @vite(['resources/css/app.css', 'resources/js/app.js'])
+                <title>@yield('title')</title>
             </head>
             <body>
                     @include('partials.header')
