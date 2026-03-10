a30d0e6493dd6472020eeed25e58d75fb2ff863a
Bertrand <bertrand.tang.dev@gmail.com>
2026-03-10 15:55:25 +0100

    retire '...' de la restriction sur la description

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         @endif
         <x-article
             :title="$article['title']"
-            :description="Str::limit($article['description'], 30, '…')"
+            :description="Str::limit($article['description'], 30)"
         />
     @empty
         <p>Aucun article a afficher pour le moment.</p>
