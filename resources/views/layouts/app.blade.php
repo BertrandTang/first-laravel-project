3179d5f9d6455c0dde5cfde0ee42041bbc656faf
Bertrand <bertrand.tang.dev@gmail.com>
2026-03-10 16:48:07 +0100

    ajout du type pour le favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
             <head>
                 <meta charset="UTF-8">
-                <link rel="icon" href="{{ asset('favicon.ico') }}">
+                <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
                 @vite(['resources/css/app.css', 'resources/js/app.js'])
                 <title>@yield('title')</title>
             </head>
